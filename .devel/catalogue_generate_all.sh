b2644db28da5b4dc628ea87b206a3b29420d540b
gagolews <m.gagolewski@gmail.com>
2022-09-10 21:12:21 +1000

    more

--- .devel/catalogue_generate_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Copyleft (C) 2018-2021 Marek Gagolewski <https://www.gagolewski.com>
-
-batteries="fcps graves other sipu uci wut mnist h2mg g2mg"
-for b in $batteries; do
-    ./catalogue_generate.py $b
-    pandoc catalogue/$b.md --to html -o catalogue/$b.html
-done
